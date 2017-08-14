a1dca4bf62c986660b8174826e78118f9632559a
Yagiz Kurt <yagiz96@hotmail.com>
2017-08-14 15:34:29 +0300

    Bitti abi
    5.02 ve 5.10 cozuldu

---
+++ Algoritmalar/example_3_0_6.m
@@ -0,0 +1,23 @@
+clear
+global mu
+mu = 398600;
+%...Input data for Example 3.6:
+ro = 10000;
+vro = 3.0752;
+dt = 3600;
+a = -19655;
+%...
+%...Pass the input data to the function kepler_U, which returns x
+%...(Universal Kepler’s requires the reciprocal of
+% semimajor axis):
+x = kepler_U(dt, ro, vro, 1/a);
+%...Echo the input data and output the results to the command window:
+fprintf('---------------------------------------------------')
+fprintf('\n Example 3.6\n')
+fprintf('\n Initial radial coordinate (km) = %g',ro)
+fprintf('\n Initial radial velocity (km/s) = %g',vro)
+fprintf('\n Elapsed time (seconds) = %g',dt)
+fprintf('\n Semimajor axis (km) = %g\n',a)
+fprintf('\n Universal anomaly (kmˆ0.5) = %g',x)
+fprintf('\n-----------------------------------------------\n')
+% ˜˜
---
+++ Algoritmalar/example_3_0_7.m
@@ -0,0 +1,23 @@
+clear
+global mu
+mu = 398600;
+R0 = [ 7000 -12124 0];
+V0 = [2.6679 4.6210 0];
+t = 3600;
+%...
+%...Algorithm 3.4:
+[R, V] = rv_from_r0v0(R0, V0, t);
+%...Echo the input data and output the results to the command window:
+fprintf('---------------------------------------------------')
+fprintf('\n Example 3.7\n')
+fprintf('\n Initial position vector (km):')
+fprintf('\n r0 = (%g, %g, %g)\n', R0(1), R0(2), R0(3))
+fprintf('\n Initial velocity vector (km/s):')
+fprintf('\n v0 = (%g, %g, %g)', V0(1), V0(2), V0(3))
+fprintf('\n\n Elapsed time = %g s\n',t)
+fprintf('\n Final position vector (km):')
+fprintf('\n r = (%g, %g, %g)\n', R(1), R(2), R(3))
+fprintf('\n Final velocity vector (km/s):')
+fprintf('\n v = (%g, %g, %g)', V(1), V(2), V(3))
+fprintf('\n-----------------------------------------------\n')
+% ˜˜˜˜
