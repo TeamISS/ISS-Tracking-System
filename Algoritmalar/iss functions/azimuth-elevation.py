25a5934e6ebd52aff95ab5077676883277ba2ba1
Yagiz Kurt <yagiz96@hotmail.com>
2017-08-23 00:37:29 +0300

    Sabit nokta gosterme bitti

---
+++ Algoritmalar/iss functions/azimuth-elevation.py
@@ -0,0 +1,79 @@
+import sys
+import os
+import math
+import numpy as np
+
+def norm(vector):
+
+    z = 0
+    for i in range(len(vector)):
+        z += (vector[i]*vector[i])           
+
+    return math.sqrt(z)
+
+def distance(initial, satellite):
+
+    dot = [x-y for x,y in zip(initial,satellite)]
+
+    for i in range(len(dot)):
+        dot[i] = dot[i]*dot[i]
+
+    dist = sum(dot)                   
+    return math.sqrt(dist)
+
+def oldrangeVector(satellite, initial):
+    
+    r = [x-y for x,y in zip(satellite, initial)]
+    return r
+
+def newrangeVector():
+
+    satellite = [3220.788, 3268.788, 4983.089]
+    biz = [4126.088, 2659.977, 4059.869]
+    vector = oldrangeVector(satellite,biz)
+
+    rx = vector[0]
+    ry = vector[1]
+    rz = vector[2]
+
+    sideReal = math.radians(228.55)
+
+    rS = math.sin(sideReal)*math.cos(0.694)*rx + math.sin(sideReal)*math.sin(0.694)*ry - math.cos(sideReal)*rz
+
+    rE = -math.sin(0.694)*rx + math.cos(0.694)*ry
+
+    rZ = math.cos(sideReal)*math.cos(0.694)*rx + math.cos(sideReal)*math.sin(0.694)*ry + math.sin(sideReal)*rz
+
+    newr = [rS, rE, rZ]
+    
+    return newr
+
+def azimuth(vector):
+
+    Az = math.atan(-vector[1]/vector[0])
+
+    return math.degrees(Az)
+
+def elevation(vector):
+
+    r = norm(vector)
+
+    el = math.asin(vector[2] / r)
+
+    return math.degrees(el)
+    
+
+
+def main():
+    satellite = [3220.788, 3268.788, 4983.089]
+    biz = [4126.088, 2659.977, 4059.869]
+    a = newrangeVector()
+    b =oldrangeVector(satellite, biz)
+
+    print norm(a), a , "Azimuth:", azimuth(a), "Elevation:" , elevation(a)
+    print norm(b), b , "Azimuth:", azimuth(b), "Elevation:" , elevation(b)
+    
+            
+         
+if __name__ == "__main__":
+        main()
